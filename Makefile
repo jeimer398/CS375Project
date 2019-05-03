e8410f16d9f573b8144bcd8383be9416bc797c67
Joseph Osgood <josgood2@binghamton.edu>
2019-05-03 16:06:46 -0400

    test file generator!

--- Makefile
+++ Makefile
@@ -11,11 +11,18 @@ bfs.o: BFS.cpp Node.h
 dfs.o: DFS.cpp Node.h
 	g++ $(CFLAGS) -c DFS.cpp -o dfs.o
 
+gen_test: gen_test.cpp
+	g++ $(CFLAGS) gen_test.cpp -o gen_test
+	./gen_test
+	make graph
+
 parse_to_jflap: parse_to_jflap.cpp
 	g++ $(CFLAGS) parse_to_jflap.cpp -o parse_to_jflap
 
 graph: parse_to_jflap
-	./parse_to_jflap tests/test1.txt jflap/test1.txt
+	./parse_to_jflap tests/bintree.txt jflap/bintree.txt
+	./parse_to_jflap tests/onecycle.txt jflap/onecycle.txt
+	./parse_to_jflap tests/random.txt jflap/random.txt
 
 run: all
 	./exe tests/test1.txt outfile1.txt
@@ -28,9 +35,18 @@ run: all
 	echo "==="
 	./exe tests/test4.txt outfile4.txt
 	cat outfile4.txt
+	echo "==="
+	./exe tests/bintree.txt bintree.txt
+	cat bintree.txt
+	echo "==="
+	./exe tests/onecycle.txt onecycle.txt
+	cat onecycle.txt
+	echo "==="
+	./exe tests/random.txt random.txt
+	cat random.txt
 
 gdb: all
 	gdb ./exe
 
 clean: 
-	rm exe *.o *.txt parse_to_jflap
+	rm exe *.o *.txt parse_to_jflap gen_test
---
+++ gen_test.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+bool isin(vector<int> vect, int findme){
+	for(auto iter = vect.begin(); iter < vect.end(); iter++){
+		if(*iter == findme){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(int argc, char ** argv){
+	string test1 = "tests/bintree.txt";
+	string test2 = "tests/onecycle.txt";
+	string test3 = "tests/manycycle.txt";
+	string test4 = "tests/random.txt";
+
+	int num_nodes = 15;
+	int MAX_EDGES = 15;
+	string outstr = "";
+
+	srand(time(NULL));
+	//int num = rand() % MAX + 1;
+
+	//bintree
+	ofstream outfile;
+	outfile.open(test1);
+	for(int i=0; i<num_nodes+1; i++){
+		outstr += to_string(i) + " ";
+		//if any edges
+		if(i < num_nodes / 2 ){
+			//add edges
+			int num_edge = 2; // rand() % MAX + 1;
+			outstr += to_string(num_edge) + " ";
+			outstr += to_string(i*2+1) + " ";
+			outstr += to_string(i*2+2);
+		}else{
+			outstr += "0";
+		}
+		outstr += "\n";
+	}
+	outfile << outstr << endl;
+	outfile.close();
+
+	//circle
+	outstr = "";
+	outfile.open(test2);
+	for(int i=0; i<num_nodes; i++){
+		outstr += to_string(i) + " ";
+		outstr += to_string(1) + " ";
+		outstr += to_string((i+1) % num_nodes) + " ";
+		outstr += "\n";
+	}
+	outfile << outstr << endl;
+	outfile.close();
+
+	//random
+	outstr = "";
+	outfile.open(test4);
+	for(int i=0; i<num_nodes; i++){
+		outstr += to_string(i) + " ";
+		//if any edges
+		int num_edge = rand() % MAX_EDGES;
+		outstr += to_string(num_edge) + " ";
+		vector<int> vect; 
+		for(int j=0; j<num_edge; j++){
+			//add edges
+			int rand_edge = rand() % num_nodes;
+			while(isin(vect, rand_edge))
+				rand_edge = rand() % num_nodes;
+			vect.push_back(rand_edge);
+			outstr += to_string(rand_edge) + " ";
+		}
+		outstr += "\n";
+	}
+	outfile << outstr << endl;
+	outfile.close();
+
+	return 0;
+}
